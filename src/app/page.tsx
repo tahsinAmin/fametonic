3dced591ec03aa83ce64e78cb6a753c19745f16a
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-07-28 01:15:59 +0600

    feat: Implement mobile hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             journey today!</span>
         </h1>
       </div>
-      <header className="flex justify-end bg-[#010101] py-[16px]">
+      <header className="fixed-header flex justify-end py-[16px]">
         <div className="header-bottom flex items-center justify-between w-[251px] pr-[28px]">
           <div className="header-bottom-left">
             <Image src="/logo.svg" alt="Logo" width={100} height={100} />
@@ -19,8 +19,54 @@ export default function Home() {
           </div>
         </div>
       </header>
-      <main className="bg-gray-400 h-[calc(100vh-152px)]">
-        <h2>Main</h2>
+      <main className="content">
+        <section className="tai-hero-section">
+          <div className="tai-hero-section-bg">
+            <div className="hero-wrapper">
+              <div className="hero-content">
+                <h1 className="hero-title text-center text-white">Want to Turn Social Media Into a Profitable Career?</h1>
+                <div className="mcdonalds-logo-wrapper text-center">
+                  <span className="mcdonalds-logo-shadow">Discover your way to success with Fametonic:</span>
+                  <span className="mcdonalds-logo-main">Discover your way to success with Fametonic:</span>
+                </div>
+                <ul className="text-white pt-[22px]">
+                  <li className="grid grid-cols-9 items-center mb-[12px]">
+                    <Image className="col-span-1" src="/sparkle.png" alt="Sparkle" width={22} height={23} />
+                    <span className="col-span-8">Start growing your influence right away—no waiting required!</span>
+                  </li>
+                  <li className="grid grid-cols-9 items-center mb-[12px]">
+                    <Image className="col-span-1" src="/sparkle.png" alt="Sparkle" width={22} height={23} />
+                    <span className="col-span-8">Create viral TikToks and Reels step by step with easy-to-follow lessons</span>
+                  </li>
+                  <li className="grid grid-cols-9 items-center mb-[12px]">
+                    <Image className="col-span-1" src="/sparkle.png" alt="Sparkle" width={22} height={23} />
+                    <span className="col-span-8">Use a Personal AI Worker to boost your content </span>
+                  </li>
+                  <li className="grid grid-cols-9 items-center mb-[12px]">
+                    <Image className="col-span-1" src="/sparkle.png" alt="Sparkle" width={22} height={23} />
+                    <span className="col-span-8">Learn from expert-led courses designed for aspiring influencers</span>
+                  </li>
+                </ul>
+                <div>
+                  <div className="text-center">
+                    <p className="text-[#ABABAB] mb-[13px] mt-[20px] text-xs">By clicking "Get Started", you agree with Terms and Conditions, Privacy Policy, Subscription Terms</p>
+                    <p className="text-[#ABABAB] text-[10px] nunito mb-[32px]">Fametonic 2025 ©All Rights Reserved.</p>
+                  </div>
+                  <div className="hero-buttons">
+                    <button className="hero-btn flex gap-[11px] items-center justify-center mb-[10px]">
+                      <span>Get Started</span>
+                      <span><Image src="/arrow.svg" alt="Arrow" width={8} height={14} /></span>
+                    </button>
+                    <p className="text-white text-center text-xs">1-minute quiz for personalized Insights</p>
+                  </div>
+                </div>
+              </div>
+              <div className="hero-image">
+                <Image width={580} height={615} src="/hero-image.png" alt="Mobile Phone" />
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
